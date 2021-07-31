d5ebe457a7aa8a37152153f796e24429c307b37a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-07-31 14:03:51 +0100

    feat: add sign in form

--- screens/public/SignIn/index.tsx
+++ screens/public/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack } from 'native-base';
+import { Box, Heading, VStack, FormControl, Input } from 'native-base';
 import { ThemeContext } from '../../../providers/Theme';
 import ThemedButton from '../../../components/ThemedButton';
 
@@ -12,13 +12,13 @@ const SignIn: React.FunctionComponent = () => {
       <VStack space={2} mt={5}>
         <FormControl>
           <FormControl.Label _text={{ color: tc.textColorScheme }}>Email</FormControl.Label>
-          <Input w="300px" variant="filled" _focus={{ borderColor: tc.textColorScheme }} />
+          <Input w="300px" _focus={{ borderColor: tc.textColorScheme }} color={tc.textColorScheme} />
         </FormControl>
         <FormControl mb={5}>
           <FormControl.Label _text={{ color: tc.textColorScheme }}>Password</FormControl.Label>
-          <Input w="300px" variant="filled" type="password" _focus={{ borderColor: tc.textColorScheme }} />
+          <Input w="300px" type="password" _focus={{ borderColor: tc.textColorScheme }} color={tc.textColorScheme} />
         </FormControl>
-        <VStack space={2}>
+        <VStack space={2} justifyContent="center" alignItems="center">
           <ThemedButton themeContext={tc}>SIGN IN</ThemedButton>
         </VStack>
       </VStack>
