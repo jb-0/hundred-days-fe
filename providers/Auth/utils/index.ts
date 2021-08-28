8fe49636b39eab23c2bc81dff0b441f6a316a7b3
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-08-28 18:15:23 +0100

    chore: remove unnecessary auth storage

--- providers/Auth/utils/index.ts
+++ providers/Auth/utils/index.ts
@@ -1,4 +1,3 @@
 export { createUser } from './createUser';
 export { signInWithEmail } from './signIn';
-export { getAsyncStoreUserCredential, setAsyncStoreUserCredential } from './asyncStorage';
 export { firebaseConfig } from './firebaseConfig';
