ae43549bcd777bb81b3b9fb652869654f6b6bcda
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-08-29 11:59:54 +0100

    chore: nest routes to facilitate access to useAuth

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { ThemeProvider, AuthProvider } from './providers';
-import Landing from './screens/public/Landing';
-import SignIn from './screens/public/SignIn';
-import Register from './screens/public/Register';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { RootStackParamList } from './Types/Navigation';
+import { RootStackParamList } from './types/Navigation';
+import Router from './router';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
@@ -13,13 +11,7 @@ export default function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
-        <NavigationContainer>
-          <RootStack.Navigator initialRouteName="landing">
-            <RootStack.Screen name="landing" component={Landing} options={{ title: 'Sign In / Register' }} />
-            <RootStack.Screen name="signIn" component={SignIn} options={{ title: 'Sign In' }} />
-            <RootStack.Screen name="register" component={Register} options={{ title: 'Register' }} />
-          </RootStack.Navigator>
-        </NavigationContainer>
+        <Router />
       </ThemeProvider>
     </AuthProvider>
   );
