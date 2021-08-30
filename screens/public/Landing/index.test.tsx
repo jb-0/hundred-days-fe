92bf3c5391bef8c3abe0161200b11244d5f02428
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-08-30 02:57:52 +0100

    test: create landing screen test cases

---
+++ screens/public/Landing/index.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, fireEvent, waitFor, act, cleanup } from '@testing-library/react-native';
+import { AppNavigationProps } from '../../../types/Navigation';
+import Landing from './';
+import { NativeBaseWrapper } from '../../../utils/testHelpers';
+
+const mockNavigation = {
+  navigate: jest.fn(),
+};
+
+describe('Screen - Landing', () => {
+  afterEach(cleanup);
+
+  it('renders landing screen with log in and register buttons', () => {
+    const { getAllByA11yRole, getByText } = render(
+      <NativeBaseWrapper>
+        <Landing navigation={{} as AppNavigationProps['landing']} />
+      </NativeBaseWrapper>,
+    );
+
+    const buttons = getAllByA11yRole('button');
+
+    expect(buttons.length).toBe(2);
+    expect(getByText('LOG IN')).toBeTruthy();
+    expect(getByText('REGISTER')).toBeTruthy();
+  });
+
+  it('clicking the log in button fires the correct navigation event', () => {
+    const { getByText } = render(
+      <NativeBaseWrapper>
+        <Landing navigation={{ ...mockNavigation } as any} />
+      </NativeBaseWrapper>,
+    );
+
+    const logInButton = getByText('LOG IN');
+    fireEvent.press(logInButton);
+
+    expect(mockNavigation.navigate).toHaveBeenCalledWith('signIn');
+  });
+
+  it('clicking the register button fires the correct navigation event', () => {
+    const { getByText } = render(
+      <NativeBaseWrapper>
+        <Landing navigation={{ ...mockNavigation } as any} />
+      </NativeBaseWrapper>,
+    );
+
+    const registerButton = getByText('REGISTER');
+    fireEvent.press(registerButton);
+
+    expect(mockNavigation.navigate).toHaveBeenCalledWith('register');
+  });
+});
