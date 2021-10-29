d3ec75b2b9071fdb4085fa50b9daafb2dcb98766
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-10-29 20:31:43 +0100

    feat: add app bottom navigation

--- src/screens/private/App/index.tsx
+++ src/screens/private/App/index.tsx
@@ -1,34 +1,57 @@
 import React from 'react';
-import { Button, Layout } from '@ui-kitten/components';
+import { BottomNavigation, BottomNavigationTab, Button, Card, Icon, Layout, Modal, Text } from '@ui-kitten/components';
 import { useTranslation } from 'react-i18next';
 import { SpinningLoader, TranslatedText } from '../../../components';
 import { useAuth } from '../../../providers';
+import LogOutModal from '../../../components/LogOutModal';
+
+const ListIcon = (props: unknown) => <Icon {...props} name="list-outline" />;
+const ChartIcon = (props: unknown) => <Icon {...props} name="pie-chart-outline" />;
+const LogOutIcon = (props: unknown) => <Icon {...props} name="log-out-outline" />;
 
 const App: React.FunctionComponent = () => {
   const { t } = useTranslation();
-  const { signOut } = useAuth();
-  const [isSigningOut, setIsSigningOut] = React.useState(false);
-
-  const handleSignOut = async () => {
-    setIsSigningOut(true);
-    await signOut();
-    setIsSigningOut(false);
-  };
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const [modal, setModal] = React.useState(false);
 
   return (
-    <Layout
-      style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingHorizontal: 20 }}
-      testID="home-page"
-    >
-      <Button onPress={handleSignOut} size="large" appearance="outline" disabled={isSigningOut} style={{ width: 150 }}>
-        {isSigningOut ? (
-          <SpinningLoader />
-        ) : (
-          <TranslatedText tKey={t('translation:screens.private.unverified.buttons.sign_out')} />
-        )}
-      </Button>
-    </Layout>
+    <>
+      <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+        <LogOutModal visible={modal} setVisibility={setModal} />
+        <Button
+          appearance="ghost"
+          accessoryLeft={LogOutIcon}
+          size="giant"
+          style={{ marginLeft: 'auto' }}
+          onPress={() => setModal(true)}
+        />
+
+        <Layout style={{ flex: 1, backgroundColor: 'red' }} testID="home-page"></Layout>
+      </Layout>
+      <BottomNavigation selectedIndex={selectedIndex} onSelect={(index) => setSelectedIndex(index)}>
+        <BottomNavigationTab title={t('translation:screens.private.app.bottom_nav.list').toString()} icon={ListIcon} />
+        <BottomNavigationTab
+          title={t('translation:screens.private.app.bottom_nav.stats').toString()}
+          icon={ChartIcon}
+        />
+      </BottomNavigation>
+    </>
   );
+
+  // return (
+  // <Layout
+  //   style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingHorizontal: 20 }}
+  //   testID="home-page"
+  // >
+  //     <Button onPress={handleSignOut} size="large" appearance="outline" disabled={isSigningOut} style={{ width: 150 }}>
+  //       {isSigningOut ? (
+  //         <SpinningLoader />
+  //       ) : (
+  //         <TranslatedText tKey={t('translation:screens.private.unverified.buttons.sign_out')} />
+  //       )}
+  //     </Button>
+  //   </Layout>
+  // );
 };
 
 export default App;
