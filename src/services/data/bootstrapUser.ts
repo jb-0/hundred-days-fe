8931e63405874f3bf2e8b28991f6ec25e30ece9a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-10-30 12:40:15 +0100

    chore: update bootstrap per schema & add entry

--- src/services/data/bootstrapUser.ts
+++ src/services/data/bootstrapUser.ts
@@ -4,14 +4,24 @@ import { models } from '../../types';
 export const bootstrapUser = async (firebaseApp: firebase.app.App, email: string): Promise<boolean> => {
   const db = firebaseApp.firestore();
   const cleanEmail = email?.toLowerCase();
+  const timestamp = new Date();
 
   try {
-    const newUserRecord: models.NewUser = {
+    const newUserRecord: models.User = {
       email: cleanEmail,
-      diary: { dateDisplayFormat: 'dd/mm/yyyy', entries: [] },
+      settings: { dateDisplayFormat: 'dd/mm/yyyy', preferredLanguage: 'en-GB' },
+    };
+
+    const sampleDiaryEntry: models.DiaryEntry = {
+      date: timestamp.toDateString(),
+      createdAt: timestamp,
+      lastUpdated: timestamp,
+      freeText: 'Welcome to your diary! This is your first entry',
     };
 
     await db.collection('users').doc(cleanEmail).set(newUserRecord);
+    await db.collection('users').doc(cleanEmail).collection('entries').doc().set(sampleDiaryEntry);
+
     return true;
   } catch (e) {
     return false;
